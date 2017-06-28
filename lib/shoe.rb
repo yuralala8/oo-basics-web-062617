c4a26fd4d16e92d9fabd57d3216b7a935ddfff02
yuralala8 <yc1435@nyu.edu>
2017-06-28 17:57:47 -0400

    basics

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+
+attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+      @brand = brand
+      @condition = condition
+      @cobble
+      end
+
+    def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+
+end
